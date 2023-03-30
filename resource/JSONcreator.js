ac43a8b1799dd25d8835df1687a984840b74b95f
Rodrigo <rodrigo.o.2012.r@gmail.com>
2023-03-29 21:03:47 -0300

    Imagenes
    Subida de imagenes + componentes

--- resource/JSONcreator.js
+++ resource/JSONcreator.js
@@ -1,15 +1,23 @@
-const argumentos = process.argv[2];
-console.log("Argumentos: ", argumentos)
+const fs = require('fs');
 console.log("Creando lista de personas...")
+const argumentos = process.argv[2];
 
 let listaPersonas = [];
+const nombres = ["Juan", "Pepito", "Emanuel", "Josefina", "Laura", "Ana", "Hortencia", "Roco", "Pablo", "María", "Patricia", "Patricio", "Julieta", "Romeo", "Eugenia", "Eugenio", "Flor", "Gustavo", "Martina", "Martha"];
+const apellidos = ["Venegas", "Rodriguez", "Saenz", "Peltrovianca", "Desconfianza", "Miruela", "Agena Gutierrez", "Guemes", "Patmarrat", "Sanchez", "Papita Suelta", "Matracio", "Murciano", "Pelado", "Lopez"]
 
-const generarLista = () => {
-    for (let i = 0; i < 2; i++) {
+const shuffle = (array) => {
+    array.sort(() => Math.random() - 0.5);
+}
+
+const generarLista = (argumentos = 5) => {
+    shuffle(nombres);
+    shuffle(apellidos);
+    for (let i = 0; i < argumentos; i++) {
         const persona = {
-            nombre: `Persona ${i}`,
-            apellido: `Apellido ${i}`,
-            img: `./img/personas/per${i}`,
+            nombre: `${nombres[i]}`,
+            apellido: `${apellidos[i]}`,
+            img: `./img/personas/per${i}.png`,
             descripcion: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
         };
         /* listaPersonas[`persona${i}`] = {
@@ -23,12 +31,19 @@ const generarLista = () => {
     console.log("Lista creada.")
 }
 
-generarLista();
+generarLista(argumentos);
 
 const recorrerLista = (() => {
     console.log("Recorriendo lista de personas...")
     /*     for (const item in listaPersonas) {
             console.log(listaPersonas[item])
         } */
-    listaPersonas.map(item => console.log(item))
+    listaPersonas.map(item => console.log(item.nombre, item.apellido))
 })();
+
+console.log("Guardando JSON..")
+
+fs.writeFile('./public/db/personas.json', JSON.stringify(listaPersonas), 'utf8', (err) => {
+    if (err) throw err;
+    console.log("Archivo guardado con éxito!")
+})
---
+++ src/components/titulo.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../App.css";
+
+const Titulo = () => {
+
+    return (
+        <div className="Titulo">
+            <div className="Titulo-Text">Busqueda de Usuario</div>
+            <div className="Titulo-Menu">
+                <ul>
+                    <li>Ejercicio 1</li>
+                    <li>Ejercicio 2</li>
+                    <li>Ejercicio 3</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Titulo;
